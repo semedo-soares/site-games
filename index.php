8e5b4127939b6a31f969cc4099c5d5802d9a9c27
semedo-soares <alvinhosemedo@outlook.com>
2022-05-03 14:47:15 -0100

    v1

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="estilos/estilo.css">
+	<link rel="stylesheet" type="text/css" href="estilos/bootstrap.min.css">
+	<title></title>
+</head>
+<body>
+	<div id="corpo">
+		<h2>Escolha o seu Jogo</h2>
+		<div id="tabela">
+			<table class="listagem">
+				<tr class="row">
+					<td class="col-md-2">Foto</td>
+					<td class="col-md-6">Nome</td>
+					<td class="col-md-4">adm</td>
+				</tr>
+				<tr  class="row">
+					<td class="col-md-2">Foto</td>
+					<td class="col-md-6">Nome</td>
+					<td class="col-md-4">adm</td>
+				</tr>
+				<tr class="row">
+					<td class="col-md-2">Foto</td>
+					<td class="col-md-6">Nome</td>
+					<td class="col-md-4">adm</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+
+</body>
+</html>
